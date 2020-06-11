227b227f0081d888970485d2039a2532bd8a67ad
Jestin Joy <jestinjoy@gmail.com>
2020-06-11 19:02:18 +0530

    Update

---
+++ logistic.py
@@ -0,0 +1,31 @@
+#Importing necessary libraries 
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+#from joblib import dump,load
+from sklearn import metrics
+
+#Reading the data
+dataset=np.loadtxt('added.csv',delimiter=",")
+
+
+#Splitting the data  into target and data
+data = dataset[:,:-1] #all columns except the last one
+
+target = dataset[:,len(dataset[0])-1] #only the last column
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3,random_state=109)
+
+#Model Training
+model =LogisticRegression(random_state=0)
+
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+
+#Model Evaluation
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred,average="macro"))
+print(metrics.precision_recall_fscore_support(y_test, y_pred, average="macro"))
+print(metrics.confusion_matrix(y_test, y_pred))
