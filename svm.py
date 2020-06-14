6a039fc95b33e3c2783a8ff452cec46d1d813c2e
Jestin Joy <jestinjoy@gmail.com>
2020-06-14 23:04:46 +0530

    SVM

---
+++ svm.py
@@ -0,0 +1,24 @@
+#Importing necessary libraries 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import csv
+from sklearn import svm
+
+
+dataset=np.loadtxt('added.csv',delimiter=",")
+
+data = dataset[:,:-1] #all columns except the last one
+target = dataset[:,len(dataset[0])-1] #only the last column
+X_train, X_test, y_train, y_test = train_test_split(data, target,test_size=0.3,random_state=109)
+
+clf = svm.SVC(kernel="rbf",decision_function_shape='ovo')
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+#Model Evaluation
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred,average="macro"))
+print(metrics.precision_recall_fscore_support(y_test, y_pred, average="macro"))
+print(metrics.confusion_matrix(y_test, y_pred))
